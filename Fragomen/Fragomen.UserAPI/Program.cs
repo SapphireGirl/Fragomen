52efd9bb2317c9ac6183d8fb375dd18a447c23b0
SapphireGirl <justinealires@outlook.com>
2026-05-07 12:02:41 -0500

    Fix Repository in UserAPI, Add Angular client

--- Fragomen/Fragomen.UserAPI/Program.cs
+++ Fragomen/Fragomen.UserAPI/Program.cs
@@ -1,28 +1,37 @@
 using Fragomen.UserAPI.Interfaces;
 using Fragomen.UserAPI.Repositories;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Register Repositories
+// In connection string: Only use Trusted_Connection=True for local development.
+// For production, set Trusted_Connection=False and
+// provide appropriate credentials in the connection string.
+var userDbConnectionString = builder.Configuration.GetConnectionString("UserAPIConnectionString");
+builder.Services.AddTransient<IDbConnection>(sp => new SqlConnection(userDbConnectionString));
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "User.API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
